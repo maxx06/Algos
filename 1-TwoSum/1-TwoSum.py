ad6b13bf3878d558b340cb3d07752ca333684be4
max xiong <67605840+maxx06@users.noreply.github.com>
2025-01-18 17:08:39 -0500

    Update file 1/18/2025, 5:08:39 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,4 +1,4 @@
 class Solution:
     def twoSum(self, n: List[int], t: int) -> List[int]:
         e=enumerate
-        return next(([i,j] for i,x in e(n) for j,y in e(n) if x+y==t and i!=j), [])
+        return next(([i,j]for i,x in e(n)for j,y in e(n)if x+y==t and i!=j),)
